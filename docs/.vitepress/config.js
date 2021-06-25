21f8f55d20236e35d8753ad76bc5f42f068048b7
Markus Siering <markus277@gmail.com>
2021-06-25 11:12:00 +0200

    docs: add github link

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -2,4 +2,8 @@ module.exports = {
   title: "Vue-Blockies",
   description: "A simple Vue3 wrapper component for ethereum-blockies",
   base: "/vue-blockies/",
+  themeConfig: {
+    repo: "herrbertling/vue-blockies",
+    lastUpdated: "Last Updated",
+  },
 }
